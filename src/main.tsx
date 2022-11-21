fbb539dc5560818f7bd271a2dc6f19707ca3aa9c
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2022-11-21 18:01:55 -0300

    fix main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store';
 import theme from './theme/Theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider theme={theme}>
-            <Provider store={store}>
-                <App />
-            </Provider>
+            <App />
         </ChakraProvider>
     </React.StrictMode>
 );
